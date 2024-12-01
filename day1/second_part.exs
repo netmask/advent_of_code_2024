34f6749cf9ef33cc1ab1af17c19398dc436a8b88
Jonathan Garay <jonathan@garay.mx>
2024-12-01 15:21:22 -0500

    Elixir solution

---
+++ day1/second_part.exs
@@ -0,0 +1,20 @@
+File.stream!("input.txt")
+|> Stream.map(&String.split(&1, "  "))
+|> Enum.reduce([[], []], fn row, acc ->
+  row
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&String.to_integer/1)
+  |> then(fn [first, second] -> [[first | Enum.at(acc, 0)], [second | Enum.at(acc, 1)]] end)
+end)
+|> Enum.map(&Enum.sort/1)
+|> then(fn [first, second] ->
+  with group <-
+         Enum.group_by(second, & &1)
+         |> Enum.map(fn {key, values} -> {key, Enum.count(values)} end)
+         |> Enum.into(%{}) do
+    first
+    |> Enum.map(fn x -> x * Map.get(group, x, 0) end)
+    |> Enum.sum()
+    |> IO.inspect()
+  end
+end)
